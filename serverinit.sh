a045f794138040581ad0d37aca79c1ae72bc58ac
scratchedguitar <31390860+scratchedguitar@users.noreply.github.com>
2021-08-17 21:50:54 +0100

    Create serverinit.sh

---
+++ serverinit.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get upgrade
+apt-get install git curl nano -y
+cd ~/
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update
+apt install gh
